c6e63ddc68c3ce3c6764fcf5ba91a7f6e0c27b51
Brian Foster <bdfoster89@gmail.com>
2016-06-09 21:54:08 -0400

    rename manual --> virtual

--- blumate/components/alarm_control_panel/demo.py
+++ blumate/components/alarm_control_panel/demo.py
@@ -4,11 +4,11 @@ Demo platform that has two fake alarm control panels.
 For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/demo/
 """
-import blumate.components.alarm_control_panel.virtual as manual
+import blumate.components.alarm_control_panel.virtual as virtual
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Demo alarm control panel platform."""
     add_devices([
-        manual.VirtualAlarm(hass, 'Alarm', '1234', 5, 10),
+        virtual.VirtualAlarm(hass, 'Alarm', '1234', 5, 10),
     ])
